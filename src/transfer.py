0dc1f20f578e781309f13f62d7e8ab81fb670f77
mixtape115 <g0115renshu@gmail.com>
2023-02-18 00:22:44 +0900

    rm big data

---
+++ src/transfer.py
@@ -0,0 +1,136 @@
+#Imports
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+from torch.utils.data import DataLoader
+import torchvision.datasets as datasets
+import torchvision
+import torchvision.transforms as transforms
+
+import cv2
+import numpy as np
+import random
+from tqdm import tqdm
+
+# Set device
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print(device)
+
+# Hyperparameters
+# input_size = 784
+in_channels = 3
+num_classes = 10
+learning_rate = 1e-4
+batch_size = 64
+num_epochs = 10
+load_model = False
+
+import sys
+
+class Identity(nn.Module):
+    def __init__(self):
+        super(Identity, self).__init__()
+
+    def forward(self, x):
+        return x
+    
+# Load pretrained model
+model = torchvision.models.vgg16(pretrained=True)
+
+for param in model.parameters():
+    param.requires_grad= False
+
+model.avgpool = Identity()
+model.classifier = nn.Sequential(nn.Linear(512, 100),
+                                 nn.ReLU(),
+                                 nn.Linear(100, 10))
+model.to(device)
+print(model)
+
+# sys.exit()
+
+def save_checkpoint(state, filename="my_checkpoint.pth.tar"):
+    print('=> Saving checkpoint')
+    torch.save(state, filename)
+
+def load_checkpoint(checkpoint):
+    print("=> Loading checkpoint")
+    model.load_state_dict(checkpoint['state_dict'])
+    optimizer.load_state_dict(checkpoint['optimizer'])
+
+
+
+# Load Data
+train_dataset = datasets.CIFAR10(root='datasets/', train=True, transform=transforms.ToTensor(), download=True)
+train_loader = DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
+
+test_dataset = datasets.CIFAR10(root='datasets/', train=False, transform=transforms.ToTensor(), download=True)
+test_loader = DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=True)
+
+
+# loss and optimizer
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+
+if load_model:
+    load_checkpoint(torch.load("my_checkpoint.pth.tar"))
+
+# Train Network
+for epoch in range(num_epochs):
+    losses = []
+
+    if epoch % 3 == 0:
+        checkpoint = {'state_dict' : model.state_dict(), 'optimizer' : optimizer.state_dict()}
+        save_checkpoint(checkpoint)
+
+    # print(f"Epoch: {epoch}")
+    for batch_idx, (data, targets) in enumerate(train_loader):
+        # Get data to cuda if possible
+        data = data.to(device=device)
+        targets = targets.to(device=device)
+
+        # Get to correct shape, 28x28->784
+        # -1 will flatten all outer dimensions into one
+        # data = data.reshape(data.shape[0], -1) 
+
+        # forward
+        scores = model(data)
+        loss = criterion(scores, targets)
+        losses.append(loss.item())
+
+        # backward
+        optimizer.zero_grad()
+        loss.backward()
+
+        # gradient descent or adam step
+        optimizer.step()
+    mean_loss = sum(losses)/len(losses)
+    print(f'loss at epoch {epoch} was {mean_loss:.5f}' )
+
+# Check accuracy on training and test to see how goodd our model
+def check_accuracy(loader, model):
+    num_correct = 0
+    num_samples = 0
+    model.eval()
+
+    with torch.no_grad():
+        for x, y in loader:
+            x = x.to(device=device)
+            y = y.to(device=device)
+            # x = x.reshape(x.shape[0], -1)
+
+            scores = model(x)
+            _, predictions = scores.max(1)
+            num_correct += (predictions == y).sum()
+            num_samples += predictions.size(0)
+
+        print(
+            f"Got {num_correct} / {num_samples} with accuracy"
+            f" {float(num_correct) / float(num_samples) * 100:.2f}"
+        )
+
+    model.train()
+
+check_accuracy(train_loader, model)
+check_accuracy(test_loader, model)
